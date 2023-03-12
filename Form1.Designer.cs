ad19c7a9b278c65d2b7c3798a106a927b1f7a2e1
JuanPabloRP <jpabloroldanp@gmail.com>
2023-03-11 20:25:26 -0500

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace hilosDeProcesos
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.tmr1 = new System.Windows.Forms.Timer(this.components);
+            this.tmr2 = new System.Windows.Forms.Timer(this.components);
+            this.tmr3 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(32, 21);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(42, 13);
+            this.lbl1.TabIndex = 0;
+            this.lbl1.Text = "Timer 1";
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Location = new System.Drawing.Point(32, 68);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(42, 13);
+            this.lbl2.TabIndex = 1;
+            this.lbl2.Text = "Timer 2";
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.Location = new System.Drawing.Point(32, 123);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(42, 13);
+            this.lbl3.TabIndex = 2;
+            this.lbl3.Text = "Timer 3";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(65, 201);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(252, 201);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 4;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // tmr1
+            // 
+            this.tmr1.Tick += new System.EventHandler(this.tmr1_Tick);
+            // 
+            // tmr2
+            // 
+            this.tmr2.Tick += new System.EventHandler(this.tmr2_Tick);
+            // 
+            // tmr3
+            // 
+            this.tmr3.Tick += new System.EventHandler(this.tmr3_Tick);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(396, 282);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lbl1);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Main form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Timer tmr1;
+        private System.Windows.Forms.Timer tmr2;
+        private System.Windows.Forms.Timer tmr3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hilosDeProcesos
+{
+    public partial class frmMain : Form
+    {
+        int counterLbl1 = 1, lbl1Velocity = 2, counterLbl2 = 1, lbl2Velocity = 2, counterLbl3 = 1, lbl2Velocity3 = 2, lbl3Velocity = 2;
+        bool lbl1WentTo100 = false, lbl1WentTo1 = true, lbl2WentTo20 = false, lbl2WentTo1 = true, lbl3WentToTheFinal = false, lbl3WentToTheBegin = true;
+
+        
+        public frmMain()
+        {
+            InitializeComponent();
+            //lbl2Velocity = lbl1Velocity * 5;
+        }
+
+        private  void btnStart_Click(object sender, EventArgs e)
+        {
+            tmr1.Enabled = true;
+            tmr2.Enabled = true;
+            //Task.Delay(10);
+            tmr3.Enabled = true;
+
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            tmr1.Enabled = false;
+            tmr2.Enabled = false;
+            tmr3.Enabled = false;
+        }
+
+        private void tmr1_Tick(object sender, EventArgs e)
+        {
+            if (counterLbl1 == 100)
+            {
+                lbl1WentTo100 = true;
+                lbl1WentTo1 = false;
+            }
+
+            if (counterLbl1 == 1)
+            {
+                lbl1WentTo100 = false;
+                lbl1WentTo1 = true;
+            }
+
+            if (lbl1WentTo100 == false && lbl1WentTo1 == true)
+            {
+                lbl1.Text = counterLbl1++.ToString();
+                lbl1.Location = new Point(lbl1.Location.X + lbl1Velocity, lbl1.Location.Y);
+            }
+            else if (lbl1WentTo100 == true && lbl1WentTo1 == false)
+            {
+                lbl1.Location = new Point(lbl1.Location.X - lbl1Velocity, lbl1.Location.Y);
+                lbl1.Text = counterLbl1--.ToString();
+            }
+            
+        }
+
+        private void tmr2_Tick(object sender, EventArgs e)
+        {
+            if (counterLbl2 == 20)
+            {
+                lbl2WentTo20 = true;
+                lbl2WentTo1 = false;
+                lbl2.Text = 20.ToString();
+            }
+
+            if (counterLbl2 == 1)
+            {
+                lbl2WentTo20 = false;
+                lbl2WentTo1 = true;
+            }
+
+            if (lbl2WentTo20 == false && lbl2WentTo1 == true)
+            {
+                lbl2.Text = counterLbl2++.ToString();
+                lbl2.Location = new Point(lbl2.Location.X + lbl2Velocity, lbl2.Location.Y);
+            }
+
+            if ((lbl2WentTo20 == true && lbl2WentTo1 == false) && (lbl1WentTo100 == true && counterLbl1 <= 20))
+            {
+                lbl2.Text = counterLbl2--.ToString();
+                lbl2.Location = new Point(lbl2.Location.X - lbl2Velocity, lbl2.Location.Y);
+            }
+
+        }
+
+
+        private void tmr3_Tick(object sender, EventArgs e)
+        {
+            
+            if (counterLbl3 == 50 ) 
+            {
+                lbl3WentToTheFinal = true;
+                lbl3WentToTheBegin = false;
+            }
+
+            if (counterLbl3 == 1 )
+            {
+                lbl3WentToTheFinal = false;
+                lbl3WentToTheBegin = true;
+            }
+
+            
+
+            if ( (lbl3WentToTheFinal == false && lbl3WentToTheBegin == true) || (counterLbl1 > counterLbl3 && counterLbl1 <= 50))
+            {
+                lbl3.Text = counterLbl3++.ToString();
+                lbl3.Location = new Point(lbl3.Location.X + lbl3Velocity, lbl3.Location.Y);
+            }
+            else if ( (lbl3WentToTheFinal == true && lbl3WentToTheBegin == false) || (counterLbl1 < counterLbl3 && counterLbl1 >= 1))
+            {
+                lbl3.Location = new Point(lbl3.Location.X - lbl3Velocity, lbl3.Location.Y);
+                lbl3.Text = counterLbl3--.ToString();
+            }
+            else
+            {
+                lbl3.Location = new Point(lbl3.Location.X, lbl3.Location.Y);
+                lbl3.Text = "Error";
+            }
+
+            
+        }
+
+    }
+}
